ea970b11919ae5a613282f05388202809dfcb29b
Jonathan Peppers <jonathan.peppers@microsoft.com>
2026-01-16 10:44:25 -0600

    Attach console in debug mode for WPF app
    Added code to attach the console to the parent process in debug mode using kernel32.dll. This enables console output for debugging purposes in the WPF application.

--- MyWpfApp/App.xaml.cs
+++ MyWpfApp/App.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Runtime.InteropServices;
 using System.Windows;
 
 namespace MyWpfApp;
@@ -9,9 +8,16 @@ namespace MyWpfApp;
 /// </summary>
 public partial class App : Application
 {
+    [DllImport("kernel32.dll")]
+    static extern bool AttachConsole(int dwProcessId);
+    const int ATTACH_PARENT_PROCESS = -1;
+
     public App()
     {
 #if DEBUG
+        AttachConsole(ATTACH_PARENT_PROCESS);
+        Console.WriteLine("Debug console attached.");
+
         VibeExtensions.VibeServer.Initialize();
 #endif
     }
